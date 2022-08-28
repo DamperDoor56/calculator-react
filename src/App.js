ee6d9e0418da819f9e0450f53291512c94f7a5e7
Ludmi Lopez <ludmit@outlook.com>
2022-08-27 22:39:00 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import calcData from './components/CalcData';
 import './styles/App.scss';
 
+const operators = ['AC', '/', 'x', '+', '-', '='];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const Display = ({ input, output }) => (
+    <div className='output'>
+      <span className='result'>{output}</span>
+      <span id='display' className='input'>{input}</span>
+    </div>
+  )
+
+  const Key = ({ keyData: { id, value }, handleInput}) =>(
+    <button id={id} onClick={() => handleInput(value)}>
+      {value}
+    </button>
+  );
+
+  const Keyboard = ({ handleInput }) => (
+    <div className='keys'>
+      {calcData.map((key) => (
+        <Key key={key.id} keyData={key} handleInput={handleInput} />
+      ))}
+    </div>
+  );
+
 function App() {
+  const [input, setInput] = useState("0");
+  const [output, setOutput] = useState("");
+
+  const handleInput = () =>{
+
+  }
+
   return (
-    <div className="App">
-      <h2>Hello Word</h2>
+    <div className='container'>
+    <div className='calculator'>
+     <Display input={input} output={output} />
+     <Keyboard handleInput={handleInput} />
+    </div>
     </div>
   );
 }
---
+++ src/components/CalcData.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const calcData = [
+    {id:'clear', value: 'AC'},
+    {id:'divide', value: '/'},
+    {id:'multiply', value: 'x'},
+    {id:'seven', value: 7},
+    {id:'eight', value: 8},
+    {id:'nine', value: 9},
+    {id:'substract', value: '-'},
+    {id:'four', value: 4},
+    {id:'five', value: 5},
+    {id:'six', value: 6},
+    {id:'add', value: '+'},
+    {id:'one', value: 1},
+    {id:'two', value: 2},
+    {id:'three', value: 3},
+    {id:'equals', value: '='},
+    {id:'zero', value: 0},
+    {id:'decimal', value: '.'},
+];
+
+
+export default calcData;
